3817b69a0e41337c4f3b51dc21b20868dc728811
KathyrineLu <katherine.tianyi.lu@gmail.com>
2020-02-19 15:49:08 -0500

    Add singlehabitviewcontroller

---
+++ cued/views/SingleHabitViewController/SingleHabitViewController.h
@@ -0,0 +1,18 @@
+//
+//  SingleHabitViewController.h
+//  cued
+//
+//  Created by Katherine Lu on 2020-02-19.
+//  Copyright © 2020 Justin Pezzack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SingleHabitViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *routineLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ cued/views/SingleHabitViewController/SingleHabitViewController.m
@@ -0,0 +1,32 @@
+//
+//  SingleHabitViewController.m
+//  cued
+//
+//  Created by Katherine Lu on 2020-02-19.
+//  Copyright © 2020 Justin Pezzack. All rights reserved.
+//
+
+#import "SingleHabitViewController.h"
+
+@interface SingleHabitViewController ()
+
+@end
+
+@implementation SingleHabitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
